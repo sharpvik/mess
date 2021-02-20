a2d179354ed9e25f48ec9324ce961eeb0f9ce959
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2021-02-20 18:33:32 +0000

    Client under construction

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,43 +1,133 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Task
+import Time
 
 
-main : Program () Int Msg
+
+-- MAIN
+
+
+main : Program Flags Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
-type alias Model =
+
+-- MODEL
+
+
+type alias Flags =
     Int
 
 
-init : Model
-init =
-    10
+type alias Model =
+    { zone : Time.Zone
+    , time : Time.Posix
+    }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init current =
+    ( Model Time.utc (Time.millisToPosix current)
+    , Task.perform AdjustTimeZone Time.here
+    )
+
+
+
+-- UPDATE
 
 
 type Msg
-    = Increment
-    | Decrement
+    = Tick Time.Posix
+    | AdjustTimeZone Time.Zone
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            model + 10
+        Tick newTime ->
+            ( { model | time = newTime }
+            , Cmd.none
+            )
 
-        Decrement ->
-            model - 1
+        AdjustTimeZone newZone ->
+            ( { model | zone = newZone }
+            , Cmd.none
+            )
 
 
-view : Model -> Html Msg
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Time.every 1000 Tick
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
+    { title = "Mess"
+    , body = body model
+    }
+
+
+body : Model -> List (Html Msg)
+body model =
+    [ header []
+        [ h1 [] [ text "Mess" ]
+        , time model
         ]
+    , section [ class "warning" ]
+        [ h1 [] [ text "Coming soon..." ]
+        , p []
+            [ text "Mess chat app is currently under construction. We're working hard to create a new way of building local communities using technology and internet."
+            ]
+        , a [ href "https://github.com/sharpvik/mess" ] [ text "Contribute" ]
+        ]
+    ]
+
+
+
+-- UTIL
+
+
+time : Model -> Html Msg
+time model =
+    let
+        hour =
+            timeFormat (Time.toHour model.zone model.time)
+
+        minute =
+            timeFormat (Time.toMinute model.zone model.time)
+
+        second =
+            timeFormat (Time.toSecond model.zone model.time)
+    in
+    pre [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
+
+
+timeFormat : Int -> String
+timeFormat i =
+    let
+        s =
+            String.fromInt i
+    in
+    if String.length s == 1 then
+        "0" ++ s
+
+    else
+        s
